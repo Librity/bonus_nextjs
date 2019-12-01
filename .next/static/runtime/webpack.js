37d4ea429f7ada74a5229bf642186eb643a65cf1
Luis Geniole <luis.geniole@gmail.com>
2019-12-01 14:25:09 -0300

    add Head&Document

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fe93d8aba7f07b4a2a66";
+/******/ 	var hotCurrentHash = "617ad34a44f960b2ac0a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
